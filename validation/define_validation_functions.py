a35923b36c9b53010069ff34155c6f393437e332
Anna Lang <anna.lang@plymbp003563.local>
2016-09-02 11:29:30 -0700

    Modified tests to start on language id

--- validation/define_validation_functions.py
+++ validation/define_validation_functions.py
@@ -6,7 +6,9 @@ Created on Mon Aug 29 12:39:48 2016
 """
 import pandas as pd
 #import pytest 
-
+from datetime import datetime
+from nltk import wordpunct_tokenize 
+from nltk.corpus import stopwords
 
 
 def is_categorical(path, categorical_cols):
@@ -19,15 +21,50 @@ def check_num_cols(path, expected_num_cols):
     n = len(d.columns)
     assert n == expected_num_cols
 
-
 def check_full(path, full_cols):
     d = pd.read_csv(path)
     d_sub = d[full_cols]
     assert d_sub.isnull().any().any() == False
   
-def date_time_makes_sense(path):
+def date_time_makes_sense(path, start_time_col, end_time_col):
     d = pd.read_csv(path)
-    start = d['start_time']
-    end = d['end_time']
-    assert start>=end
+    start = pd.to_datetime(d[start_time_col])
+    end = pd.to_datetime(d[end_time_col])
+    assert (start >= end).all
     
+def check_for_English(path):
+    d = pd.read_csv(path)
+    d_chat = d.loc[d['Widget_Action'] == 'enter_text']
+    text = d_chat['Action_content'].astype('str')    
+    for line in text:
+        tokens = wordpunct_tokenize(line)
+    words = [word.lower() for word in tokens]
+    stopwords_set = set(stopwords.words('English'))
+    words_set = set(words)
+    common_elements = words_set.intersection(stopwords_set)
+    assert (common_elements > 0).all
+
+
+  
+    
+    
+
+
+d = pd.read_csv('/Users/anna.lang/Work/repo/ds_sandbox_ail/validation/NUI_sandbox_Session.csv')
+p = '/Users/anna.lang/Work/repo/ds_sandbox_ail/validation/NUI_sandbox_Session.csv'
+date_time_makes_sense(p, 'Date_time_start', 'Date_time_end')
+
+chat_p = '/Users/anna.lang/Work/repo/ds_sandbox_ail/validation/NUI_sandbox_Chat_screen.csv'
+chat_d = pd.read_csv(chat_p)
+d_chat = chat_d.loc[chat_d['Widget_Action'] == 'enter_text']
+text = d_chat['Action_content'].astype('str')
+
+for line in text:
+    tokens = wordpunct_tokenize(line)
+
+
+words = [word.lower() for word in tokens]
+stopwords_set = set(stopwords.words('English'))
+words_set = set(words)
+common_elements = words_set.intersection(stopwords_set)
+assert (common_elements > 0)
--- validation/run_validation_functions.py
+++ validation/run_validation_functions.py
@@ -11,7 +11,7 @@ Created on Tue Aug 30 13:07:12 2016
 import pytest
 from define_validation_functions import *
 
-def run_tests(path, categorical_cols, num_cols, full_cols): 
+def run_tests_Iris(path, categorical_cols, num_cols, full_cols): 
     is_categorical(path, categorical_cols)
     check_num_cols(path, num_cols)
     check_full(path, full_cols)
@@ -20,13 +20,13 @@ def run_tests(path, categorical_cols, num_cols, full_cols):
 Iris_pth = "/Users/anna.lang/Work/repo/ds_sandbox_ail/validation/iris.csv"
 Iris_Cats_list = ['Species']
 Iris_n_cols = 6
-Iris_no_blanks_cols = ['Species', 'Sepal.Length', 'Sepal.Width', 'Petal.Length']
-run_tests(Iris_pth, test_list, num_cols, full_cols)
+Iris_full_cols = ['Species', 'Sepal.Length', 'Sepal.Width', 'Petal.Length']
+run_tests_Iris(Iris_pth, Iris_Cats_list, Iris_n_cols, Iris_full_cols)
 
 #Simple Session Data test case 
 Sess_pth = "/Users/anna.lang/Work/repo/ds_sandbox_ail/validation/NUI_sandbox_Session.csv"
 Sess_num_cols = 8
 Sess_full_cols = ['Session_id', 'Date_time_start', 'Date_time_end','Data_feed_v']
-run_tests(Sess_pth, full_cols, Sess_num_cols, Sess_full_cols)
+run_tests_Session(Sess_pth, Sess_full_cols, Sess_num_cols, Sess_full_cols)
 
 
